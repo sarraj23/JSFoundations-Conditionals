0faa73ae3abb229d4def557249eca7271f4a90e9
sarraj23 <78157490+sarraj23@users.noreply.github.com>
2021-02-02 19:39:53 +0300

    Solved the conditionals task

--- calculator.js
+++ calculator.js
@@ -1,3 +1,19 @@
 // Define the numbers and the operation here
 
+let firstnumber = 23
+let secondnumber = 11
+let operation = "+"
+
 // Write if-statements here
+
+if (operation === "+") 
+{console.log (firstnumber + secondnumber)}
+
+else if (operation === "-")
+{console.log (firstnumber - secondnumber)}
+
+else if (operation === "*")
+{console.log (firstnumber * secondnumber)}
+
+else if (operation === "/")
+{console.log (firstnumber / secondnumber)}
